fb41cadc3f087115960cc9006250e64d109cc817
hekai000 <675974119@qq.com>
2018-04-18 10:16:52 +0800

    add pycrawl about multiprocess

---
+++ multiprocessingPoolTest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os, time, random
+from multiprocessing import Pool
+
+
+def run_task(name):
+
+    print 'Task %s （%s）Runing' % (name, os.getpid())
+    time.sleep(random.random()*3)
+    print "Task %s end" % name
+
+
+if __name__ == "__main__":
+    print "Current process %s." % os.getpid()
+    p = Pool(processes=3)
+    for i in range(5):
+        p.apply_async(run_task, args=(i,))
+    print "Waiting for all subprocessing done"
+    p.close()
+    p.join()
+    print "All Process end"
